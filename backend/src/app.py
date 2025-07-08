bd4303a1f35ad051b167b04bbacfa2f4c53a6302
JBras <52011942+BrasJ@users.noreply.github.com>
2025-07-07 21:37:34 -0700

    Fix minor bugs in backend
    - Refactor backend/src/routes/challenge.py to correct name typo
    - Installed clerk-backend-api dependancy

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from .routes import challenge, webhooks
 
 app = FastAPI()
 
-app.middleware(
-                CORSMiddleware,
-                allow_origins=["*"],
-                allow_credentials=True,
-                allow_methods=["*"],
-                allow_header=["8"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_header=["8"]
 )
+
+app.include_router(challenge.router, prefix="/api")
